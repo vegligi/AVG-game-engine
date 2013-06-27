16ec9956a931668b0f69e317f8abc910b591652b
vegligi <join.vegligi@gmail.com>
2013-06-27 17:04:49 +0200

    0.42
    temp save

---
+++ dilemma/GameMode.h
@@ -0,0 +1,17 @@
+//
+//  GameMode.h
+//  dilemma
+//
+//  Created by TANG YANG on 6/27/13.
+//  Copyright (c) 2013 TANG YANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+@class User;
+
+@interface GameMode : NSManagedObject
+
+@property (nonatomic) BOOL *rpg;
+
+@end
---
+++ dilemma/GameMode.m
@@ -0,0 +1,15 @@
+//
+//  GameMode.m
+//  dilemma
+//
+//  Created by TANG YANG on 6/27/13.
+//  Copyright (c) 2013 TANG YANG. All rights reserved.
+//
+
+#import "GameMode.h"
+
+@implementation GameMode
+
+@dynamic rpg;
+
+@end
